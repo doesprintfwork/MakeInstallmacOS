bf0e3b47d86fada51e7cb039527f1801bba773fd
Mojave <mojave@Mojavede-iMac-Pro.local>
2019-08-16 21:32:13 +0800

    Update MakeAppMacOS.py

--- MakeAppMacOS.py
+++ MakeAppMacOS.py
@@ -19,12 +19,28 @@ def clear():
 neededfiles = [r"../AppleDiagnostics.chunklist", r"../AppleDiagnostics.dmg", r"../BaseSystem.chunklist", r"../BaseSystem.dmg", r"../InstallESDDmg.pkg", r"../InstallInfo.plist"]
 
 def packapp():
-    pass
+
+    title("Choose macOS Version")
+    print("1: High Sierra")
+    print("2: Mojave")
+    print("Q: Quit")
+    print("M: Main Menu")
+    option = input("Please enter an option: ")
+    version = ""
+    if option == "1":
+        version = "High Sierra"
+    elif option == "2":
+        version = "Mojave"
+    elif version == "Q":
+        quit()
+
+    os.system("hdiutil attach ../BaseSystem.dmg")
+    shutil.copy(r"/Volumes/macOS Base System/Install macOS")
 
 def convert():
     pass
 
-def main():
+def checkfiles():
     clear()
     title("Checking Required Files...")
     time.sleep(0.5)
@@ -32,19 +48,29 @@ def main():
         if isfile(f) == False:
             print("Missing Files.")
             sys.exit()
+
+def quit():
+    print("Goodbye! Have a good day!")
+    sys.exit()
+
+def mainmenu():
     clear()
-    
     title("Main Menu")
     print("1: Pack files to an Install macOS Application")
     print("2: Convert Network Recovery macOS Installer USB to a Full Installer")
     print("Q: Quit")
     option = input("Enter an option: ")
     if option == "Q" or option == "q":
-        print("Goodbye! Have a good day!")
-        sys.exit()
+        quit()
     elif option == "1":
         packapp()
     elif option == "2":
         convert()
+    else:
+        mainmenu()
+
+def main():
+    packapp()
+
 
 main()
