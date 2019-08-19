73e8fd7eb93a0b76229f4b8750037a838ec44aff
Mojave <mojave@Mojavede-iMac-Pro.local>
2019-08-19 10:39:54 +0800

    Create MakeSharedSupportWin.py

---
+++ MakeSharedSupportWin.py
@@ -0,0 +1,90 @@
+import os, shutil, plistlib, time, sys
+
+line = "--------------------------------------------------"
+
+def noline(string):
+    print(string,end="")
+
+def title(string):
+    print(line)
+    print("{:^50}".format(string))
+    print(line)
+
+def isfile(string):
+    return os.path.isfile(string)
+
+def clear():
+    os.system("cls")
+
+def copyfiles(sharedsupportloc):
+    for f in neededfiles:
+        noline("    Copying {}... ".format(f))
+
+        shutil.copy(f, sharedsupportloc)
+        print("Done.")
+
+def checkfiles():
+    clear()
+    title("Checking Required Files...")
+    time.sleep(0.5)
+    for f in neededfiles:
+        if isfile(f) == False:
+            print("Missing Files.")
+            sys.exit()
+    time.sleep(1)
+
+neededfiles = [r"../AppleDiagnostics.chunklist", r"../AppleDiagnostics.dmg", r"../BaseSystem.chunklist", r"../BaseSystem.dmg", r"../InstallESDDmg.pkg", r"../InstallInfo.plist"]
+
+def editplist():
+    fp = open(r"./InstallInfo.plist","rb")
+    installinfo = plistlib.load(fp)
+    del installinfo["Payload Image Info"]["chunklistURL"]
+    del installinfo["Payload Image Info"]["chunklistid"]
+    installinfo["Payload Image Info"]["URL"] = "InstallESD.dmg"
+    installinfo["Payload Image Info"]["id"] = "com.apple.dmg.InstallESD"
+    plistlib.dump(installinfo, open(r"./InstallInfo.plist", "wb"))
+
+def SharedSupport():
+    clear()
+
+    title("Packing files to SharedSupport")
+
+    noline("Making Directories... ")
+    os.mkdir("SharedSupport")
+    print("Done.")
+
+    noline("Copying files... ")
+    copyfiles(r"./SharedSupport")
+    print("Done.")
+
+    noline("Editting InstallInfor.plist... ")
+    os.chdir(r"./SharedSupport")
+    editplist()
+    os.rename("InstallESDDmg.pkg", "InstallESD.dmg")
+    print("Done")
+
+    print("All Done.")
+    time.sleep(1)
+
+    mainmenu()
+
+def mainmenu():
+    clear()
+    title("Main Menu")
+    print("P: Pack files to a folder (SharedSupport)")
+    print("Q: Quit")
+    option = input("Enter an option: ")
+    if option == "Q" or option == "q":
+        quit()
+    elif option == "P" or option == "p":
+        SharedSupport()
+    else:
+        mainmenu()
+
+def main():
+    os.chdir("./")
+    checkfiles()
+    mainmenu()
+
+if __name__ == "__main__":
+    main()
